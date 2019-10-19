d5e86d64e3db48a85850dadf73916f2985a81e4e
Nikko Mina <nmina@rccl.com>
2019-10-19 19:57:31 -0400

    Initial service commit.

---
+++ nasa-hackathon-service/src/main/java/com/example/satelite/nasahackathonservice/NasaHackathonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.satelite.nasahackathonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaHackathonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaHackathonServiceApplication.class, args);
+	}
+
+}
